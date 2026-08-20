d87c7cd7f158947c598f16c8faf3820a7ba6d68e
Paul Gian Calgao Corpuz <paca.corpuz.au@phinmaed.com>
2026-08-20 23:44:11 +0800

    Add files via upload
    P1 Part 2 - Python Receipt Laboratory

---
+++ store_receipt.py
@@ -0,0 +1,49 @@
+
+name=input("Enter Customer Name: ")
+contact=input("Enter Customer contact no.: ")
+address=input("Enter Customer Address: ")
+
+product1=input("Enter Product 1: ")
+price1=float(input("Enter Price: "))
+quantity1=int(input("Enter Quantity: "))
+amount1 = price1 * quantity1
+
+product2=input("Enter Product 2: ")
+price2=float(input("Enter price: "))
+quantity2=int(input("Enter Quantity: "))
+amount2 = price2 * quantity2
+
+product3=input("Enter Product 3: ")
+price3=float(input("Enter Price: "))
+quantity3=int(input("Enter Quantity: "))
+amount3 = price3 * quantity3
+
+subtotal = amount1 + amount2 + amount3
+
+discount=float(input("Enter discount (%): "))
+discount_amount = subtotal * (discount/100)
+total = subtotal - discount_amount
+
+print("======STORE RECEIPT======")
+print(("name"), name)
+print(("contact"), contact)
+print(("address"),address)
+print("------------------------")
+print("product    price    quantity    amount")
+print(product1, price1, quantity1, amount1)
+print(product2, price2, quantity2, amount2)
+print(product3, price3, quantity3, amount3)
+print("------------------------")
+print(("subtotal:"), subtotal)
+print(("discount:"), discount)
+print("------------------------")
+print(("TOTAL:"), total)
+
+
+
+
+
+
+
+
+
